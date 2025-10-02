db3dbf2d012c6573e745c43fb8b4ddc1fd5015b6
aremusulaiman2002-dotcom <aremusulaiman2002@gmail.com>
2025-10-02 17:54:06 +0100

    fix: update homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,21 +148,6 @@ export default function Home() {
             </motion.div>
           </motion.div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div 
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        >
-          <div className="w-6 h-10 border-2 border-slate-400 rounded-full flex justify-center">
-            <motion.div 
-              className="w-1 h-3 bg-slate-400 rounded-full mt-2"
-              animate={{ y: [0, 12, 0] }}
-              transition={{ duration: 2, repeat: Infinity }}
-            />
-          </div>
-        </motion.div>
       </section>
 
       {/* Trust Bar */}
